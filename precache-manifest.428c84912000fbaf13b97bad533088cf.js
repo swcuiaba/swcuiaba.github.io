03d69987f2f2cc93332f53507422f9096a424c03
Maxsuel Maccari <maxmaccari@hotmail.com>
2018-12-06 00:01:49 -0300

    Deploy SWHelper website

---
+++ precache-manifest.428c84912000fbaf13b97bad533088cf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "ed96ae562ec653635a6a",
+    "url": "/js/chunk-vendors.21a41ead.js"
+  },
+  {
+    "revision": "b2c15edd521a2644e579",
+    "url": "/js/app.6f2d7225.js"
+  },
+  {
+    "revision": "5d9ffddaee13e535d788f7b7a27db769",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ed96ae562ec653635a6a",
+    "url": "/css/chunk-vendors.96d1351d.css"
+  },
+  {
+    "revision": "b2c15edd521a2644e579",
+    "url": "/css/app.ad87ef66.css"
+  }
+];
